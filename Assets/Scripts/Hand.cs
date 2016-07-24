89d7ebb2567f6bb1c52a7cd57335a053c724eef8
bengolds <theotherben@gmail.com>
2016-07-23 22:06:26 -0700

    Can highlight over the button

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -1,19 +1,37 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Hand : MonoBehaviour {
 	private SteamVR_Controller.Device device;
+	private List<Collider> colliders;
 
 	// Use this for initialization
 	void Start () {
 		var index = GetComponentInParent<SteamVR_TrackedObject> ().index;
 		device = SteamVR_Controller.Input ((int)index);
+
+		colliders = new List<Collider> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
-			Debug.Log ("Trigger clicked");
+		}
+
+		foreach (var coll in colliders) {
+			Debug.Log (coll.name);
 		}
 	}
+
+	void OnTriggerEnter(Collider other) {
+		if (!colliders.Contains (other)) {
+			colliders.Add (other);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (colliders.Contains (other)) {
+			colliders.Remove (other);
+		}
+	}		
 }
--- Assets/Scripts/VRButton.cs
+++ Assets/Scripts/VRButton.cs
@@ -2,14 +2,62 @@
 using System.Collections;
 
 public class VRButton : MonoBehaviour {
+	public enum ButtonState {
+		Normal,
+		Highlighted,
+		Pressed
+	}
+
+	public ButtonState state;
+	public Material normalMaterial;
+	public Material highlightedMaterial;
+	public Material pressedMaterial;
+	private MeshRenderer meshRenderer;
 
 	// Use this for initialization
 	void Start () {
-	
+		meshRenderer = GetComponent<MeshRenderer> ();
+		state = ButtonState.Normal;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		switch (state) {
+		case ButtonState.Normal:
+			meshRenderer.material = normalMaterial;
+			break;
+		case ButtonState.Highlighted:
+			meshRenderer.material = highlightedMaterial;
+			break;
+		case ButtonState.Pressed:
+			meshRenderer.material = pressedMaterial;
+			break;
+		default:
+			break;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.GetComponent<Hand> () != null) {
+			OnButtonEnter ();
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.GetComponent<Hand> () != null) {
+			OnButtonExit ();
+		}
+	}
+
+	public void OnButtonEnter() {
+		state = ButtonState.Highlighted;
+	}
+
+	public void OnButtonExit() {
+		state = ButtonState.Normal;
+	}
+
+	public void OnButtonPress() {
 	
 	}
 }
