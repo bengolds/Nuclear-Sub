7efc3a6df828db4016d4ae774b384c941b7d8cc7
bengolds <theotherben@gmail.com>
2016-07-23 20:45:29 -0700

    Fixed a bug with only one controller showing up

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour {
+	private SteamVR_Controller.Device device;
+
+	// Use this for initialization
+	void Start () {
+		var index = GetComponentInParent<SteamVR_TrackedObject> ().index;
+		device = SteamVR_Controller.Input ((int)index);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Trigger clicked");
+		}
+	}
+}
