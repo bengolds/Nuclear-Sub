8b0f9d2bca61e0294c7f6cb1263f60eca44d18b3
bengolds <theotherben@gmail.com>
2016-07-25 13:18:02 -0700

    Key is better! Solution: fuck mesh colliders.

--- Assets/LockGuide.cs
+++ Assets/LockGuide.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 
 public class LockGuide : MonoBehaviour {
-	public float lockDepth = 0.04f;
+	public float lockDepth = 0.02f;
 	public GameObject snapPoint;
+	public GameObject lockBody;
 
 	// Use this for initialization
 	void Start () {
@@ -16,22 +17,25 @@ public class LockGuide : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.GetComponent<Key> () != null && GetComponent<ConfigurableJoint>() == null) {	
-			other.transform.rotation = transform.rotation;
-			Vector3 keyCenterPosition = transform.position;
-			keyCenterPosition.x = other.transform.position.x;
-			other.transform.position = snapPoint.transform.position;
+		if (other.GetComponent<Key> () != null && lockBody.GetComponent<ConfigurableJoint>() == null) {
+			SnapKeyToPosition (other.gameObject);	
 
-			var joint = gameObject.AddComponent<ConfigurableJoint> ();
-			ConfigureJoint (joint);
-			joint.connectedBody = other.attachedRigidbody;
+			var joint = lockBody.AddComponent<ConfigurableJoint> ();
+			ConfigureJoint (joint, other.attachedRigidbody);
+
+			MakeKeyTrigger (other.gameObject);
 		}
 	}
 
-	void ConfigureJoint(ConfigurableJoint joint) {
-		joint.angularXMotion = ConfigurableJointMotion.Limited;
-		joint.angularYMotion = ConfigurableJointMotion.Limited;
-		joint.angularZMotion = ConfigurableJointMotion.Limited;
+	void SnapKeyToPosition(GameObject key) {
+		key.transform.rotation = snapPoint.transform.rotation;
+		key.transform.position = snapPoint.transform.position;
+	}
+
+	void ConfigureJoint(ConfigurableJoint joint, Rigidbody connectedBody) {
+		joint.angularXMotion = ConfigurableJointMotion.Locked;
+		joint.angularYMotion = ConfigurableJointMotion.Locked;
+		joint.angularZMotion = ConfigurableJointMotion.Locked;
 		joint.xMotion = ConfigurableJointMotion.Limited;
 		joint.yMotion = ConfigurableJointMotion.Locked;
 		joint.zMotion = ConfigurableJointMotion.Locked;
@@ -39,11 +43,24 @@ public class LockGuide : MonoBehaviour {
 		var linearLimit = new SoftJointLimit ();
 		linearLimit.limit = lockDepth; 
 		joint.linearLimit = linearLimit;
+
+		joint.connectedBody = connectedBody;
+
+		joint.enableCollision = false;
+	}
+
+	void MakeKeyTrigger(GameObject key) {
+		key.GetComponent<Collider> ().isTrigger = true;
+	}
+
+	void MakeKeyCollider(GameObject key) {
+		key.GetComponent<Collider> ().isTrigger = false;
 	}
 
 	void OnTriggerExit(Collider other) {
 		if (other.GetComponent<Key> () != null) {
-			Destroy (GetComponent<ConfigurableJoint> ());
+			Destroy (lockBody.GetComponent<ConfigurableJoint> ());
+			MakeKeyCollider (other.gameObject);
 		}
 	}
 }
