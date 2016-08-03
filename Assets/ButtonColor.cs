5e9d9bf55b7b8270b8c444bb0e5d873dffe91ceb
bengolds <theotherben@gmail.com>
2016-08-02 21:08:37 -0700

    Laid out the panel and greebled the heck out of it.

--- Assets/ButtonColor.cs
+++ Assets/ButtonColor.cs
@@ -4,13 +4,21 @@ using System.Collections;
 public class ButtonColor : MonoBehaviour {
     public Color buttonColor = Color.white;
     public Color markingColor = Color.black;
+    public MeshRenderer buttonBase;
 
     void OnValidate()
     {
-        GetComponentInChildren<TextMesh>().color = markingColor;
-        var mat = GetComponent<MeshRenderer>().sharedMaterial;
-        mat.SetColor("_BaseColor", buttonColor);
-        mat.SetColor("_MarkingColor", markingColor);
+        var textMesh = GetComponentInChildren<TextMesh>();
+        if (textMesh != null)
+        {
+            textMesh.color = markingColor;
+        }
+        if (buttonBase != null)
+        {
+            var mat = buttonBase.GetComponent<MeshRenderer>().sharedMaterial;
+            mat.SetColor("_BaseColor", buttonColor);
+            mat.SetColor("_MarkingColor", markingColor);
+        }
     }
 	
 	// Update is called once per frame
