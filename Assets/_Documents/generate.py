b4aea1a3ec70109149a1a46e55cb5000172acd98
bengolds <theotherben@gmail.com>
2016-08-08 15:34:04 -0700

    Mission document generator in, temp sounds too

---
+++ Assets/_Documents/generate.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys
+import csv
+import os
+import re
+import shutil
+
+data_filename = sys.argv[1];
+html_filename = sys.argv[2];
+with open(data_filename, 'rb') as csvfile:
+	with open (html_filename, 'r') as htmlfile:
+		html = htmlfile.read()
+		data_reader = csv.reader(csvfile)
+		template_values = data_reader.next()
+
+		temp_directory = 'temp'
+		if not os.path.exists(temp_directory):
+			os.makedirs(temp_directory)
+		images_directory = 'images'	
+		if not os.path.exists(images_directory):
+			os.makedirs(images_directory)
+
+		for row in data_reader:
+			subbed = html
+			i = 0
+			for subvalue in template_values:
+				subbed = re.sub(subvalue, row[i], subbed)
+				i = i+1
+			outfilename = row[0] + ".html"
+			with open (outfilename, 'w') as outfile:
+				outfile.write(subbed)
+			outimagename = images_directory + "/" + row[0] + ".png"
+
+			cmd = "wkhtmltoimage --width 816 --height 1056 --disable-smart-width  " + outfilename + " " + outimagename
+			print(cmd)
+			os.system(cmd)
+			os.remove(outfilename)
+
