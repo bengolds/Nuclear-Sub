0ade4cc22bc9fc8d57d72d2d296a564234d22e42
bengolds <theotherben@gmail.com>
2016-08-11 17:05:30 -0700

    Hot jesus the first sound is in! and i can stop it with a button

---
+++ Assets/_Scripts/Wwise Triggers/AkButtonTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AkButtonTrigger : AkTriggerBase
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        var buttonScript = GetComponent<VRButton>();
+        if (buttonScript)
+        {
+            buttonScript.onPress.AddListener(OnButtonPress);
+        }
+    }
+
+    void OnButtonPress()
+    {
+        if (triggerDelegate != null)
+        {
+            triggerDelegate(null);
+        }
+    }
+}
