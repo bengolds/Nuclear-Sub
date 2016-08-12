10a6fa78b09dcaf920c57748eb7032c386798657
bengolds <theotherben@gmail.com>
2016-08-12 12:32:30 -0700

    Target profile is in the safe now

--- Assets/_Scripts/SafeDoor.cs
+++ Assets/_Scripts/SafeDoor.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 
 public class SafeDoor : MonoBehaviour {
 
+    public Collider safeBody;
 	// Use this for initialization
 	void Start () {
-	
+        Physics.IgnoreCollision(GetComponent<Collider>(), safeBody);
 	}
 	
 	// Update is called once per frame
