ced385ff5cd7160a2038dec337e801030b6a497c
bengolds <theotherben@gmail.com>
2016-08-17 14:03:50 -0700

    Light now updates its own emission.

--- Assets/EmissiveLight.cs
+++ Assets/EmissiveLight.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class PulsingLight : MonoBehaviour {
-
+public class EmissiveLight : MonoBehaviour {
+    [ColorUsage(false, true, 0, 100, 1/8, 3)]
+    public Color color;
     public float frequency;
     private Color onColor;
     private Renderer emRenderer;
@@ -11,14 +12,14 @@ public class PulsingLight : MonoBehaviour {
 	void Start () {
         emRenderer = GetComponent<Renderer>();
         onColor = emRenderer.material.GetColor("_EmissionColor");
-        Debug.Log(onColor);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //TODO MAKE THIS PROPER MATH EH
         float t = Mathf.Sin(frequency * Time.time);
-        Color currColor = Color.LerpUnclamped(Color.black, onColor, t);
-        DynamicGI.SetEmissive(emRenderer, currColor); 
+        Color currColor = Color.Lerp(Color.black, color, t);
+        DynamicGI.SetEmissive(emRenderer, currColor);
+        emRenderer.sharedMaterial.SetColor("_EmissionColor", currColor); 
 	}
 }
