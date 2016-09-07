629a03cd714b258c61702a08cef723239b759621
bengolds <theotherben@gmail.com>
2016-09-06 19:44:29 -0700

    Better wire stuff

---
+++ Assets/_Scripts/Ignition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ignition : HasWatchableBool {
+
+    public PowerTerminal powerTerminal;
+
+    public void TryTurnOn()
+    {
+        if (powerTerminal.isPoweredOn)
+        {
+            boolValue = true;
+        }
+    }
+}
