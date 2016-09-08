47b6ab7f600d262d60ce3a3a0b99af0ecd0762bc
bengolds <theotherben@gmail.com>
2016-09-08 14:38:40 -0700

    added reset rope button

---
+++ Assets/RegenerateRopeWrapper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(UltimateRope))]
+public class RegenerateRopeWrapper : MonoBehaviour {
+
+    private UltimateRope rope;
+	// Use this for initialization
+	void Start () {
+        rope = GetComponent<UltimateRope>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Regenerate(bool bResetNodePositions)
+    {
+        rope.Regenerate(bResetNodePositions);
+    }
+}
