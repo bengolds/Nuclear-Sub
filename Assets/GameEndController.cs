ab38b06e47d97ffd2e2f18b10cc262b508a633dd
bengolds <theotherben@gmail.com>
2016-09-15 18:29:37 -0700

    Completely replaced almost all text meshes
    now using Typogenic. it's a little better, not perfect

---
+++ Assets/GameEndController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class GameEndController : MonoBehaviour {
+    public Light mainLight;
+    public float dimIntensity;
+    public float dimDuration;
+    public Ease dimEase;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void EndGame()
+    {
+        mainLight.DOIntensity(dimIntensity, dimDuration).SetEase(dimEase);
+        DOTween.To(x => RenderSettings.ambientIntensity = x, RenderSettings.ambientIntensity, 0, dimDuration).SetEase(dimEase);
+    }
+}
