b22119307de3f80f676bd668fb4b5b23c9aaaa77
bengolds <theotherben@gmail.com>
2016-09-15 17:09:08 -0700

    Made buttons darker

--- Assets/Countdown.cs
+++ Assets/Countdown.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.Events;
 
 public class Countdown : MonoBehaviour {
     public int startingMinutes = 60;
+    public UnityEvent onCountdownEnd;
     private float startTime;
     private TextMesh textMesh;
+    private bool countdownEnded = false;
 
 	// Use this for initialization
 	void Start () {
@@ -18,5 +21,10 @@ public class Countdown : MonoBehaviour {
         float timeLeft = startingMinutes * 60 - (Time.time - startTime);
         TimeSpan timeSpan = TimeSpan.FromSeconds(timeLeft);
         textMesh.text = string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+        if (countdownEnded && timeLeft < 0)
+        {
+            countdownEnded = true;
+            onCountdownEnd.Invoke();
+        }
     }
 }
--- Assets/_Materials/ButtonShader.shader
+++ Assets/_Materials/ButtonShader.shader
@@ -33,12 +33,14 @@
 		void surf (Input IN, inout SurfaceOutputStandard o) {
 			// Albedo comes from a texture tinted by color
 			fixed4 c = tex2D (_MainTex, IN.uv_MainTex);
+			o.Emission = c * _EmissionColor;
 			c = lerp(_MarkingColor, _BaseColor, c.r);
 			o.Albedo = c.rgb;
 			// Metallic and smoothness come from slider variables
 			o.Metallic = _Metallic;
 			o.Smoothness = _Glossiness;
-			o.Emission = step(0.5, c.r) * _EmissionColor;
+			//o.Emission = step(0.5, c.r) * _EmissionColor;
+			//o.Emission = _EmissionColor;
 			o.Alpha = c.a;
 		}
 		ENDCG
