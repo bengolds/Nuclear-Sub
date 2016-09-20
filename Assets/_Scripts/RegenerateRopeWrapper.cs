d0de3372f1dc1fa623827f4f48e0a587efb3c978
bengolds <theotherben@gmail.com>
2016-09-19 17:30:42 -0700

    Rewrote the "regenerate rope" code so it won't crash

--- Assets/_Scripts/RegenerateRopeWrapper.cs
+++ Assets/_Scripts/RegenerateRopeWrapper.cs
@@ -1,13 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 [RequireComponent(typeof(UltimateRope))]
 public class RegenerateRopeWrapper : MonoBehaviour {
 
     private UltimateRope rope;
-	// Use this for initialization
-	void Start () {
+    private Dictionary<GameObject, Vector3> origPositions;
+    private Dictionary<GameObject, Quaternion> origRotations;
+    private List<GameObject> resetObjects;
+
+    // Use this for initialization
+    void Start () {
         rope = GetComponent<UltimateRope>();
+        origPositions = new Dictionary<GameObject, Vector3>();
+        origRotations = new Dictionary<GameObject, Quaternion>();
+        resetObjects = new List<GameObject>();
+
+        resetObjects.AddRange(FindObjectsOfType<UltimateRopeLink>().Select(link => link.gameObject));
+        resetObjects.Add(rope.RopeStart);
+        resetObjects.AddRange(rope.RopeNodes.Select(node => node.goNode));
+        foreach (var go in resetObjects)
+        {
+            origPositions[go] = go.transform.position;
+            origRotations[go] = go.transform.rotation;
+        }
 	}
 	
 	// Update is called once per frame
@@ -17,6 +35,16 @@ public class RegenerateRopeWrapper : MonoBehaviour {
 
     public void Regenerate(bool bResetNodePositions)
     {
-        rope.Regenerate(bResetNodePositions);
+        foreach (var go in resetObjects)
+        {
+            go.transform.position = origPositions[go];
+            go.transform.rotation = origRotations[go];
+            var rb = go.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+        }
     }
 }
