f0364a717d993f25d788fca543e29d5bc4043fad
bengolds <theotherben@gmail.com>
2016-09-26 16:26:54 -0700

    Dev tools should be disabled in builts

---
+++ Assets/_Scripts/DisableInBuild.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableInBuild : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    if (Application.isEditor)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
